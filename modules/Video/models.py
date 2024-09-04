7f49c764a6edf6d3d6cdf5e602b84bd2bca6414e
杨鑫 <10155098+escaflowne@user.noreply.gitee.com>
2024-09-04 16:17:37 +0800

    2024-09-03

---
+++ modules/Video/models.py
@@ -0,0 +1,14 @@
+from dvadmin.system.models import *
+
+
+class VideoIntroductionClipCutData(CoreModel):
+    task_name = models.CharField(max_length=255, verbose_name="任务名称", null=True, blank=True)
+    task_id = models.CharField(max_length=30, verbose_name="任务ID", null=True, blank=True)
+    p_task_id = models.CharField(max_length=30, verbose_name="关联任务ID", null=True, blank=True)
+    name = models.CharField(max_length=255, verbose_name="目录名称", null=True, blank=True)
+    txt = models.TextField(verbose_name="解说文案", null=True, blank=True)
+    tag = models.CharField(max_length=30, verbose_name="序号任务", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "视频介绍应用"
+        verbose_name_plural = verbose_name
