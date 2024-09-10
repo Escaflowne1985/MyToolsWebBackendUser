09bcd37dc2baa4046a220284a3d3a85b2d2fe864
杨鑫 <10155098+escaflowne@user.noreply.gitee.com>
2024-09-10 14:10:53 +0800

    2024-09-03

--- modules/Video/models.py
+++ modules/Video/models.py
@@ -2,9 +2,9 @@ from dvadmin.system.models import *
 
 
 class VideoIntroductionClipCutData(CoreModel):
+    level = models.TextField(verbose_name="层级", null=True, blank=True)
     task_name = models.CharField(max_length=255, verbose_name="任务名称", null=True, blank=True)
     task_id = models.CharField(max_length=30, verbose_name="任务ID", null=True, blank=True)
-    p_task_id = models.CharField(max_length=30, verbose_name="关联任务ID", null=True, blank=True)
     name = models.CharField(max_length=255, verbose_name="目录名称", null=True, blank=True)
     txt = models.TextField(verbose_name="解说文案", null=True, blank=True)
     tag = models.CharField(max_length=30, verbose_name="序号任务", null=True, blank=True)
