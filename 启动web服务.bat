9955338b4602f491629165e635faf883fc2f3e23
杨鑫 <10155098+escaflowne@user.noreply.gitee.com>
2024-09-11 10:16:21 +0800

    2024-09-10

--- 启动web服务.bat
+++ 启动web服务.bat
@@ -1,4 +1,3 @@
-.\dv3admin\Scripts\pip.exe install -r req_new.txt
 .\dv3admin\python.exe manage.py makemigrations
 .\dv3admin\python.exe manage.py migrate
 .\dv3admin\python.exe manage.py runserver 0.0.0.0:9000
