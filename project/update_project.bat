55e758661c295e289ef4bb81c25786937da31cc4
杨鑫 <10155098+escaflowne@user.noreply.gitee.com>
2024-09-22 16:46:15 +0800

    2024-09-18

--- project/update_project.bat
+++ project/update_project.bat
@@ -1,4 +1,6 @@
 git checkout .
 git pull
+.\dv3admin\python.exe manage.py makemigrations
+.\dv3admin\python.exe manage.py migrate
 .\dv3admin\Scripts\pip.exe install -r .\project\req_new.txt
 .\project\vite-project.exe
