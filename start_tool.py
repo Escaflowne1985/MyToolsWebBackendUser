3ebe54757626deb005357b280348927e8afe30ca
杨鑫 <10155098+escaflowne@user.noreply.gitee.com>
2024-09-27 10:34:09 +0800

    2024-09-27

--- start_tool.py
+++ start_tool.py
@@ -17,8 +17,11 @@ class Launcher(QtWidgets.QWidget):
         # 创建按钮
         self.kill_button = QtWidgets.QPushButton("杀掉所有Python和ffmpeg进程", self)
         self.git_button = QtWidgets.QPushButton("更新Git仓库", self)
-        self.install_button = QtWidgets.QPushButton("安装pip和数据库迁移", self)
+        self.install_button = QtWidgets.QPushButton("安装pip", self)
+        self.migrate_button = QtWidgets.QPushButton("数据库迁移", self)
         self.start_button = QtWidgets.QPushButton("启动Django服务", self)
+        self.start_client_button = QtWidgets.QPushButton("启动客户端", self)
+        self.start_client_button.setEnabled(False)  # 初始状态禁用
 
         # 创建输出区域
         self.output_area = QtWidgets.QPlainTextEdit(self)
@@ -30,7 +33,9 @@ class Launcher(QtWidgets.QWidget):
         button_layout.addWidget(self.kill_button)
         button_layout.addWidget(self.git_button)
         button_layout.addWidget(self.install_button)
+        button_layout.addWidget(self.migrate_button)
         button_layout.addWidget(self.start_button)
+        button_layout.addWidget(self.start_client_button)
 
         # 主布局
         main_layout = QtWidgets.QHBoxLayout()
@@ -43,8 +48,11 @@ class Launcher(QtWidgets.QWidget):
         self.kill_button.clicked.connect(self.kill_processes)
         self.git_button.clicked.connect(lambda:self.run_command("git checkout . && git pull"))
         self.install_button.clicked.connect(
-            lambda:self.run_command("./dv3admin/Scripts/pip.exe install -r project/req_new.txt && ./dv3admin/python.exe manage.py makemigrations && ./dv3admin/python.exe manage.py migrate"))
-        self.start_button.clicked.connect(lambda:self.run_command("./dv3admin/pythonw.exe manage.py runserver 0.0.0.0:9000"))
+            lambda:self.run_command(".\\dv3admin\\Scripts\\pip.exe install -r project\\req_new.txt"))
+        self.migrate_button.clicked.connect(
+            lambda:self.run_command(".\\dv3admin\\python.exe manage.py makemigrations && .\\dv3admin\\python.exe manage.py migrate"))
+        self.start_button.clicked.connect(lambda:self.run_django_server(".\\dv3admin\\python.exe manage.py runserver 0.0.0.0:9000"))
+        self.start_client_button.clicked.connect(lambda:self.run_command(".\\project\\vite-project.exe"))
 
     def append_output(self, message):
         self.output_area.appendPlainText(message.strip())
@@ -56,6 +64,19 @@ class Launcher(QtWidgets.QWidget):
         process.readyReadStandardOutput.connect(lambda:self.append_output(str(process.readAllStandardOutput(), 'utf-8')))
         process.readyReadStandardError.connect(lambda:self.append_output(str(process.readAllStandardError(), 'utf-8')))
         process.start()
+
+    def run_django_server(self, cmd):
+        process = QtCore.QProcess(self)
+        process.setProgram("cmd.exe")
+        process.setArguments(["/C", cmd])
+        process.readyReadStandardOutput.connect(lambda:self.check_django_startup(str(process.readAllStandardOutput(), 'utf-8')))
+        process.readyReadStandardError.connect(lambda:self.append_output(str(process.readAllStandardError(), 'utf-8')))
+        process.start()
+
+    def check_django_startup(self, output):
+        self.append_output(output.strip())
+        if "development server at" in output:
+            self.start_client_button.setEnabled(True)
 
     def kill_processes(self):
         current_process = psutil.Process()  # 获取当前进程
