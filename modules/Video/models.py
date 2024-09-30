fc1cb1b1ca82c11eee56d93998f9e6a8f3d08bd9
杨鑫 <10155098+escaflowne@user.noreply.gitee.com>
2024-09-30 18:03:29 +0800

    2024-09-30

--- modules/Video/models.py
+++ modules/Video/models.py
@@ -12,3 +12,21 @@ class VideoIntroductionClipCutData(CoreModel):
     class Meta:
         verbose_name = "视频介绍应用"
         verbose_name_plural = verbose_name
+
+
+class VideoRepeatFunAsr(CoreModel):
+    level = models.TextField(verbose_name="层级", null=True, blank=True)
+    task_name = models.CharField(max_length=255, verbose_name="任务名称", null=True, blank=True)
+    task_id = models.CharField(max_length=30, verbose_name="任务ID", null=True, blank=True)
+    order_id = models.IntegerField(verbose_name="任务数据序号", null=True, blank=True)
+    status_use = models.BooleanField(verbose_name="是否使用", default=1, null=True, blank=True)
+    status_rewrite = models.BooleanField(verbose_name="是否洗稿", default=1, null=True, blank=True)
+    speaker = models.CharField(max_length=30, verbose_name="说话人", null=True, blank=True)
+    start_time = models.CharField(max_length=100, verbose_name="开始时间", null=True, blank=True)
+    end_time = models.CharField(max_length=100, verbose_name="结束时间", null=True, blank=True)
+    srt_text = models.CharField(max_length=255, verbose_name="字幕", null=True, blank=True)
+    rewrite_text = models.CharField(max_length=255, verbose_name="修改文案", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "短剧复述剧情+解说"
+        verbose_name_plural = verbose_name
