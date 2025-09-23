9800f0649bab8e1ad8ea034d0937bc0fc325be27
Escaflowne1985 <33034782@qq.com>
2025-09-23 16:48:40 +0800

    重新整合开放自有API接入。
    Signed-off-by: Escaflowne1985 <33034782@qq.com>

--- modules/Config/apps.py
+++ modules/Config/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ConfigConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'modules.Config'
+    label = "config"  # 供 apps.get_model("Config","Configuration") 使用
